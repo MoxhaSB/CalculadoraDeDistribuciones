7aad34e815cf0dd432a805bc5549c5bdf26afa49
Alex Pérez <129556633+MoxhaSB@users.noreply.github.com>
2024-06-09 14:18:14 -0400

    Binomial finished

--- src/Objects/Binomial.java
+++ src/Objects/Binomial.java
@@ -3,7 +3,7 @@ package Objects;
 import java.util.Scanner;
 
 /**
- * Clase Binomial, contiene los calculos de una distribución binomial
+ * Clase Binomial, contiene los cálculos de una distribución binomial
  */
 public class Binomial extends HelperImpl{
 
@@ -20,12 +20,9 @@ public class Binomial extends HelperImpl{
     }
 
     /**
-     * Metodo que contiene los cálculos de la distribucion binomial
+     * Método que contiene los cálculos de la distribución binomial
      */
     public void binomial() {
-        int n = 0;
-        String p = null;
-
         while (true) {
             System.out.println("""
                     \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,6 +31,7 @@ public class Binomial extends HelperImpl{
                     
                     X ~ BINOMIAL ( n , p )
                     
+                    |+| x : número de éxitos en "n" ensayos
                     |+| n : cantidad de repeticiones del experimento
                     |+| p : probabilidad de éxito
                     
@@ -51,162 +49,217 @@ public class Binomial extends HelperImpl{
                 }
                 case 1:
                     //calcular la probabilidad
-                    calcularProbabilidad(n,p);
+                    calcularProbabilidad();
                     break;
                 case 2:
-                    //se calcula la esperanza
-                   calcularEsperanza(n,p);
+                    //calcular la esperanza
+                    calcularEsperanza();
                     break;
-
                 case 3:
-                    //calcualr la varianza
-                    calcularVarianza(n,p);
+                    //calcular la varianza
+                    calcularVarianza();
                     break;
-
                 case 4:
-                //se retorna para salir
-                {return;}
-
+                    //salir
+                    return;
                 default:
                     System.out.println("\nIngrese una opción válida.");
             }
-
         }
     }
 
     /**
-     * Metodd que calcula la probabilidad según un "n" y "p" ingresados
-     * @param n cantidad de repeticiones del experimento
-     * @param p probabilidad de éxito del intento
+     * Método que calcula la probabilidad según un "n" y "p" ingresados
      */
-    public void calcularProbabilidad(int n, String p){
+    public void calcularProbabilidad(){
         while(true) {
             System.out.println("""
                                 \n------------------------------------
                                    Calcular Probabilidad Binomial
                                 ------------------------------------
                                    
-                                      C(n,x) * p^x * (1-p)^(n-x)
+                                     P(X = x) = C(n, x) * p^x * (1-p)^(n-x)
                                                             
                                 Ingrese valor n: 
                                 """);
 
-            n = validarEntero();
+            //se lee el dato
+            int n = validarEntero();
 
+            //si es menor a 0 (no puede ser negativo)
             if (n < 0) {
                 System.out.println("\n|*| \"n\"  debe ser un número entero NO negativo");
-            } else {
-                System.out.println("Valor p (escribir como 0,0012 (con coma) ): ");
-                p = read.next();
-                double pp = convertStringToDouble(p);
 
-                if (pp < 0 || pp > 1) {
+            } else {
+
+                //se pide el valor de la probabilidad
+                System.out.println("Valor p (escribir como 0,0012 (con coma) ): ");
+                String pStr = read.next();//se lee como String
+                double p = convertStringToDouble(pStr); //se transforma a Double
+
+                //el valor de p tiene que estar entre 0 y 1
+                if (p < 0 || p > 1) {
                     System.out.println("\n|*| \"p\" debe ser un número NO negativo ni mayor a 1");
                 } else {
 
-                    double resultado = 0;
-
+                    double resultado;
+                    //se pide la la probabilidad a calcular
                     switch (validarProb()) {
+                        //CASO X<?
                         case 1 -> {
+
                             while (true) {
                                 System.out.println("\nIngrese el número a calcular (x < ?): ");
-                                int numero = validarEntero();
-                                //si no es un numero
-                                if (numero == -1) {
+                                int x = validarEntero();
+
+                                //si ingresó un valor No numérico
+                                if (x == -1) {
                                     System.out.println("\nIngrese un valor numérico.");
 
-                                    //si x es mayor a n
-                                } else if (numero < 0) {
+                                    //si ingresó un valor negativo
+                                } else if (x < 0) {
                                     System.out.println("\nEl valor de \"x\" no puede ser negativo");
-                                } else if (numero > n) {
+
+                                    //si x es mayor a n (por problemas de la combinatoria)
+                                } else if (x > n) {
                                     System.out.println("\nEl valor de \"x\" no puede ser mayor al valor de \"n\"");
 
-                                    //cualquier otro caso
+                                    //si está bien, recién se calcula la probabilidad
                                 } else {
                                     resultado = 0;
-                                    for (int i = 0; i < numero; i++) {
-                                        resultado += combinatoria(n, i).doubleValue() * Math.pow(pp, i) * Math.pow(1 - pp, n - i);
+                                    for (int i = 0; i < x; i++) {
+                                        resultado += probabilidad(n, i, p);
                                     }
-                                    System.out.println("\nLa probabilidad es : " + resultado);
+                                    System.out.println("\nLa probabilidad es: " + resultado);
                                     break;
-
                                 }
                             }
                             return;
                         }
+                        //CASO X<=?
                         case 2 -> {
+
                             while (true) {
                                 System.out.println("\nIngrese el número a calcular (x <= ?): ");
-                                int numero = validarEntero();
-                                if (numero == -1) {
+                                int x = validarEntero();
+
+                                //si se ingresó un valor NO numérico
+                                if (x == -1) {
                                     System.out.println("\nIngrese un valor numérico.");
+
+                                    //si se ingresó un valor negativo
+                                } else if (x < 0) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser negativo");
+
+                                    //si x es mayor a n (problemas de combinatoria)
+                                } else if (x > n) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser mayor al valor de \"n\"");
+
+                                    //si está bien, recién se calcula la probabilidad
                                 } else {
                                     resultado = 0;
-                                    for (int i = 0; i <= numero; i++) {
-                                        resultado += combinatoria(n, i).doubleValue() * Math.pow(pp, i) * Math.pow(1 - pp, n - i);
+                                    for (int i = 0; i <= x; i++) {
+                                        resultado += probabilidad(n, i, p);
                                     }
-                                    System.out.println("\nLa probabilidad es : " + resultado);
+                                    System.out.println("\nLa probabilidad es: " + resultado);
                                     break;
                                 }
                             }
                             return;
                         }
+                        //CASO X = ?
                         case 3 -> {
                             while (true) {
                                 System.out.println("\nIngrese el número a calcular (x = ?): ");
-                                int numero = validarEntero();
-                                if (numero == -1) {
+                                int x = validarEntero();
+
+                                //si se ingresó un valor NO numérico
+                                if (x == -1) {
                                     System.out.println("\nIngrese un valor numérico.");
+
+                                    //si se ingresó un valor negativo
+                                } else if (x < 0) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser negativo");
+
+                                    //si x es mayor a n (problemas de combinatoria)
+                                } else if (x > n) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser mayor al valor de \"n\"");
+
+                                    //si está bien, recién se calcula la probabilidad
                                 } else {
-                                    resultado = combinatoria(n, numero).doubleValue() * Math.pow(pp, numero) * Math.pow(1 - pp, n - numero);
-                                    System.out.println("\nLa probabilidad es : " + resultado);
+                                    resultado = probabilidad(n, x, p);
+                                    System.out.println("\nLa probabilidad es: " + resultado);
                                     return;
                                 }
                             }
                         }
+                        //CASO X > ?
                         case 4 -> {
+
                             while (true) {
                                 System.out.println("\nIngrese el número a calcular (x > ?): ");
-                                int numero = validarEntero();
-                                if (numero == -1) {
+                                int x = validarEntero();
+
+                                //si se ingresó un valor NO numérico
+                                if (x == -1) {
                                     System.out.println("\nIngrese un valor numérico.");
+
+                                    //si se ingresó un valor negativo
+                                } else if (x < 0) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser negativo");
+
+                                    //si x es mayor a n (problemas de combinatoria)
+                                } else if (x > n) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser mayor al valor de \"n\"");
+
+                                    //si está bien, recién se calcula la probabilidad
                                 } else {
                                     resultado = 0;
-                                    for (int i = numero + 1; i <= n; i++) {
-                                        resultado += combinatoria(n, i).doubleValue() * Math.pow(pp, i) * Math.pow(1 - pp, n - i);
+                                    for (int i = x + 1; i <= n; i++) {
+                                        resultado += probabilidad(n, i, p);
                                     }
-                                    if (resultado == 0) {
-                                        System.out.println("\nLa probabilidad es 0");
-                                    } else {
-                                        System.out.println("\nLa probabilidad es : " + resultado);
-                                    }
+                                    System.out.println("\nLa probabilidad es: " + resultado);
                                     break;
                                 }
                             }
                             return;
                         }
+                        //CASO X >= ?
                         case 5 -> {
+
                             while (true) {
                                 System.out.println("\nIngrese el número a calcular (x >= ?): ");
-                                int numero = validarEntero();
-                                if (numero == -1) {
-                                    System.out.println("\nIngrese un valor numérico positivo.");
+                                int x = validarEntero();
+
+                                //si se ingresó un valor NO numérico
+                                if (x == -1) {
+                                    System.out.println("\nIngrese un valor numérico.");
+
+                                    //si se ingresó un valor negativo
+                                } else if (x < 0) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser negativo");
+
+                                    //si x es mayor a n (problemas con combinatoria)
+                                } else if (x > n) {
+                                    System.out.println("\nEl valor de \"x\" no puede ser mayor al valor de \"n\"");
+
+                                    //si está bien, recién se calcula la probabilidad
                                 } else {
                                     resultado = 0;
-                                    for (int i = numero; i <= n; i++) {
-                                        resultado += combinatoria(n, i).doubleValue() * Math.pow(pp, i) * Math.pow(1 - pp, n - i);
+                                    for (int i = x; i <= n; i++) {
+                                        resultado += probabilidad(n, i, p);
                                     }
-                                    System.out.println("\nLa probabilidad es : " + resultado);
+                                    System.out.println("\nLa probabilidad es: " + resultado);
                                     break;
                                 }
                             }
                             return;
                         }
+                        //CASO -1 para salir
                         case -1 -> {
                             return;
                         }
                     }
-                    break;
                 }
             }
         }
@@ -214,10 +267,8 @@ public class Binomial extends HelperImpl{
 
     /**
      * Método que calcula la esperanza de una distribución binomial
-     * @param n cantidad de repeticiones del experimento
-     * @param p probabilidad de éxito del intento
      */
-    public void calcularEsperanza(int n,String p){
+    public void calcularEsperanza(){
         while(true) {
             System.out.println("""
                                 \n------------------------------------
@@ -228,19 +279,24 @@ public class Binomial extends HelperImpl{
                                                             
                                 Ingrese valor n: 
                                 """);
-            n = validarEntero();
+            int n = validarEntero();
 
-            if (n < 0) {
-                System.out.println("\n|*| \"n\"  debe ser un número entero NO negativo");
+            if(n ==-1) {
+                System.out.println("\nIngrese un valor numérico.");
+
+                //si se ingresó un valor negativo
+            }else if (n < 0) {
+                System.out.println("\n|*| \"n\"  debe ser un número positivo");
+
             } else {
                 System.out.println("Valor p (escribir como 0,0012 (con coma) ): ");
-                p = read.next();
-                double pp = convertStringToDouble(p);
+                String pStr = read.next();//se guarda en string
+                double p = convertStringToDouble(pStr); //se trasnforma a double (con formato decimal(comas))
 
-                if (pp < 0 || pp > 1) {
+                if (p < 0 || p > 1) {
                     System.out.println("\n|*| \"p\" debe ser un número NO negativo ni mayor a 1");
                 } else {
-                    System.out.println("\nLa esperanza es : " + n * pp);
+                    System.out.println("\nLa esperanza es: " + n * p);
                     break;
                 }
             }
@@ -249,37 +305,51 @@ public class Binomial extends HelperImpl{
 
     /**
      * Método que calcula la varianza de la distribución binomial
-     * @param n cantidad de repeticiones del experimento
-     * @param p probabilidad de éxito del intento
      */
-    public void calcularVarianza(int n, String p){
+    public void calcularVarianza(){
         while(true) {
             System.out.println("""
                                 \n------------------------------------
                                       Calcular Varianza Binomial
                                 ------------------------------------
                                 
-                                      VAR[X] = n * p * ( 1 - p )
+                                      VAR[X] = n * p * (1 - p)
                                                             
                                 Ingrese valor n: 
                                 """);
-            n = validarEntero();
+            int n = validarEntero();
+            if(n ==-1) {
+                System.out.println("\nIngrese un valor numérico.");
 
-            if (n < 0) {
-                System.out.println("\n|*| \"n\"  debe ser un número entero NO negativo");
+                //si se ingresó un valor negativo
+            }else if (n < 0) {
+                System.out.println("\n|*| \"n\"  debe ser un número positivo");
+
             } else {
                 System.out.println("Valor p (escribir como 0,0012 (con coma) ): ");
-                p = read.next();
-                double pp = convertStringToDouble(p);
+                String pStr = read.next();
+                double p = convertStringToDouble(pStr);
 
-                if (pp < 0 || pp > 1) {
+                if (p < 0 || p > 1) {
                     System.out.println("\n|*| \"p\" debe ser un número NO negativo ni mayor a 1");
                 } else {
-                    System.out.println("\nLa varianza es : " + +((n*pp)*(1-pp)));
+                    System.out.println("\nLa varianza es: " + (n * p * (1 - p)));
                     break;
                 }
             }
         }
     }
 
+    /**
+     * Método que calcula la probabilidad binomial
+     * @param n cantidad de intentos
+     * @param x cantidad a comparar -> x<1 x>=6
+     * @param p probabilidad
+     * @return la probabilidad calculada
+     */
+    public double probabilidad(int n, int x, double p){
+        return combinatoria(n, x).doubleValue() * Math.pow(p, x) * Math.pow(1 - p, n - x);
+    }
+
 }
+
