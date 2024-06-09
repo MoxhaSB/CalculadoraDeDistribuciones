8847365dab5f5ee088492dbe95a67201c4ab5ff9
Alex Pérez <129556633+MoxhaSB@users.noreply.github.com>
2024-06-09 12:45:27 -0400

    adding package Interfaces with Helper and Sistema

--- src/Interfaces/Helper.java
+++ src/Interfaces/Helper.java
@@ -1,2 +1,36 @@
-package Interfaces;public interface Helper {
+package Interfaces;
+
+import java.math.BigInteger;
+
+public interface Helper {
+
+    /**
+     * Metodo que valida si se ingreso bien la probabilidad para calcularla
+     *
+     * @return true si está bien, false si no
+     */
+    int validarProb();
+
+    /**
+     * Metodo que calcula una combinatoria con BigInteger (numero demasiado grandes)
+     * @param n valor de arriba de combinatoria
+     * @param k valor de abajo de combinatoria
+     * @return el valor de la combinatoria
+     */
+    BigInteger combinatoria(int n, int k);
+
+    /**
+     * Metodo que calcula el valor de un factorial con  BigInteger
+     * @param numero numero a calcular el factorial
+     * @return el factorial
+     */
+    BigInteger factorial(int numero);
+
+    /**
+     * Metodo que verifica si un texto ingresado contiene letras o no
+     * @return el numero ingresado o -1
+     */
+    int validarEntero();
+
+
 }
--- src/Interfaces/Sistema.java
+++ src/Interfaces/Sistema.java
@@ -1,61 +1,12 @@
-import java.math.BigInteger;
+package Interfaces;
+
 
 public interface Sistema {
-    /**
-     * Metodo que contiene y entrega valores con la distribución binomial
-     */
-    void binomial();
-
-    /**
-     * Metodo que contiene y entrega valores con la distribución binomial negativa
-     */
-    void binomialNegativa();
-
-    /**
-     * Metodo que contiene y entrega valores con la distribución hipergeometrica
-     */
-    void hipergeometrica();
-
-    /**
-     * Metodo que contiene y entrega valores con la distribución Poisson
-     */
-    void poisson();
-
-    /**
-     * Metodo que contiene y entrega valores con la distribución exponencial
-     */
-    void exponencial();
 
     /**
      * Metodo que contiene y lleva a las calculadoras de distribucioens
      */
     void menuPrincipal();
 
-    /**
-     * Metodo que valida si se ingreso bien la probabilidad para calcularla
-     *
-     * @return true si está bien, false si no
-     */
-    int validarProb();
 
-    /**
-     * Metodo que calcula una combinatoria con BigInteger (numero demasiado grandes)
-     * @param n valor de arriba de combinatoria
-     * @param k valor de abajo de combinatoria
-     * @return el valor de la combinatoria
-     */
-    BigInteger combinatoria(int n, int k);
-
-    /**
-     * Metodo que calcula el valor de un factorial con  BigInteger
-     * @param numero numero a calcular el factorial
-     * @return el factorial
-     */
-    BigInteger factorial(int numero);
-
-    /**
-     * Metodo que verifica si un texto ingresado contiene letras o no
-     * @return el numero ingresado o -1
-     */
-    int validarEntero();
 }
